35f7ffdbbd82f48c7fd12401d98fefe705f7daea
Zokkyen <stephane.swaenepoel@wanadoo.fr>
2022-06-02 23:10:00 +0200

    Add anime rotation + footer

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,23 @@
+/* Animation of my picture */
 
+let rotateImg = () => {
+    rotateAnimation.animate([
+        { transform: 'rotate(45deg)' },
+        { transform: 'rotate(135deg)' },
+        { transform: 'rotate(225deg)' },
+        { transform: 'rotate(315deg)' }, 
+    ], {
+        duration: 3000
+    });
+
+    pictureAnimation.animate([
+        { transform: 'rotate(315deg)' },
+        { transform: 'rotate(225deg)' },
+        { transform: 'rotate(135deg)' },
+        { transform: 'rotate(45deg)' }
+    ], {
+        duration: 3000
+    });
+}
+
+document.addEventListener("DOMContentLoaded", rotateImg);
