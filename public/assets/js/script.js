3477de95a5d63617125ff130d7a2cbc7c5abb4d3
Zokkyen <stephane.swaenepoel@wanadoo.fr>
2022-06-03 00:12:33 +0200

    Add somes information

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,17 +6,19 @@ let rotateImg = () => {
         { transform: 'rotate(135deg)' },
         { transform: 'rotate(225deg)' },
         { transform: 'rotate(315deg)' }, 
+        { transform: 'rotate(405deg)' }, 
     ], {
-        duration: 3000
+        duration: 2000
     });
 
     pictureAnimation.animate([
-        { transform: 'rotate(315deg)' },
+        { transform: 'rotate(315deg)' }, 
         { transform: 'rotate(225deg)' },
         { transform: 'rotate(135deg)' },
-        { transform: 'rotate(45deg)' }
+        { transform: 'rotate(45deg)' },
+        { transform: 'rotate(-45deg)' }
     ], {
-        duration: 3000
+        duration: 2000
     });
 }
 
