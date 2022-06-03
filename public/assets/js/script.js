80db8b78c8313688f1c1868786e73b8eafef0786
Stephane SWAENEPOEL <102791292+Zokkyen@users.noreply.github.com>
2022-06-03 12:22:52 +0200

    add effect on menu button

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,14 +5,14 @@ let rotateImg = () => {
         { transform: 'rotate(45deg)' },
         { transform: 'rotate(135deg)' },
         { transform: 'rotate(225deg)' },
-        { transform: 'rotate(315deg)' }, 
-        { transform: 'rotate(405deg)' }, 
+        { transform: 'rotate(315deg)' },
+        { transform: 'rotate(405deg)' },
     ], {
         duration: 2000
     });
 
     pictureAnimation.animate([
-        { transform: 'rotate(315deg)' }, 
+        { transform: 'rotate(315deg)' },
         { transform: 'rotate(225deg)' },
         { transform: 'rotate(135deg)' },
         { transform: 'rotate(45deg)' },
@@ -21,5 +21,22 @@ let rotateImg = () => {
         duration: 2000
     });
 }
-
 document.addEventListener("DOMContentLoaded", rotateImg);
+
+/* Animation of the menu */
+
+let menuAnimation = document.querySelector('.navbar-toggler-icon')
+menuAnimation.addEventListener('click', () => {
+    let counter = 300;
+    let navLinks = document.querySelectorAll('.nav-link')
+    navLinks.forEach(element => {
+        element.style.display = "none";
+        setTimeout(() => {
+            gsap.from(element, {duration: 0.5, x: 800})
+            element.style.display = "block";
+            gsap.to(element, {duration: 0.5, x: 10});
+        }, counter);
+        counter += 200;
+    });
+    counter = 0;
+})
